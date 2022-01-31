919dc402342fbcbbbda342de198ebcba22e8d5a1
Gustavoramoslopez <88283150+Gustavoramoslopez@users.noreply.github.com>
2022-01-31 16:48:21 -0300

    Add files via upload

---
+++ clase2.R
@@ -0,0 +1,7 @@
+library(readxl)
+setwd("C:/Users/guill")
+getwd()
+setwd("C:/Users/guill/Desktop/R/Edición Mayo-20210513T131315Z-001/Edición Mayo/Anexos")
+getwd()
+ventas <- read.csv("Ventas_corto.csv")
+#ver distintos modos de seleccionar filas
